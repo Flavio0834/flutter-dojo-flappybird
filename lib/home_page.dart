0f8e60ce79dcd64e38ecf1ec9552b1ae06c4b73b
Flavio <flaviosestu@yahoo.fr>
2022-09-21 20:50:23 +0200

    Score display + Moving bird

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+
 import 'bird.dart';
 import 'pipe.dart';
+import 'score.dart';
 
 final _pipeWidth = 80.0;
 
@@ -12,55 +16,71 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var _birdY = 0.0;
+  var _gameStarted = false;
+
+  void _startGame() {
+    _gameStarted = true;
+    Timer.periodic(const Duration(milliseconds: 10), (timer) {
+      setState(() {
+        _birdY -= 0.05;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          flex: 3,
-          child: Container(
-            color: Colors.blue,
+    return GestureDetector(
+      onTap: _gameStarted ? null : _startGame,
+      child: Column(
+        children: [
+          Expanded(
+            flex: 3,
             child: Container(
-              child: Stack(
-                children: [
-                  Container(
-                    alignment: const Alignment(-0.5, 0),
-                    child: const Bird(
-                      birdSize: 50,
-                    ),
-                  ),
-                  Pipe(
-                    pipeHeight: 150,
-                    pipeWidth: _pipeWidth,
-                    xPipeAlignment: 0.5,
-                  ),
-                  Pipe(
-                    pipeHeight: 300,
-                    pipeWidth: _pipeWidth,
-                    xPipeAlignment: 0.5,
-                    isBottomPipe: true,
-                  ),
-                  Container(
-                    alignment: const Alignment(0, -0.3),
-                    child: const Text(
-                      'T A P  T O  P L A Y',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
+              color: Colors.blue,
+              child: Container(
+                child: Stack(
+                  children: [
+                    Container(
+                      alignment: Alignment(-0.5, _birdY),
+                      child: const Bird(
+                        birdSize: 50,
                       ),
                     ),
-                  ),
-                ],
+                    Pipe(
+                      pipeHeight: 150,
+                      pipeWidth: _pipeWidth,
+                      xPipeAlignment: 0.5,
+                    ),
+                    Pipe(
+                      pipeHeight: 300,
+                      pipeWidth: _pipeWidth,
+                      xPipeAlignment: 0.5,
+                      isBottomPipe: true,
+                    ),
+                    Container(
+                      alignment: const Alignment(0, -0.3),
+                      child: const Text(
+                        'T A P  T O  P L A Y',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
-        ),
-        Expanded(
-          child: Container(
-            color: Colors.brown,
-          ),
-        )
-      ],
+          Expanded(
+            child: Container(
+              color: Colors.brown,
+              child: const GameScore(),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
