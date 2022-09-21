0ad64c2377befb2db05f896e2885b0c4cc9bfc7a
Flavio <flaviosestu@yahoo.fr>
2022-09-21 20:10:45 +0200

    Adding pipes + text 'TAP TO PLAY'

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'bird.dart';
+import 'pipe.dart';
+
+final _pipeWidth = 80.0;
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -16,11 +19,41 @@ class _HomePageState extends State<HomePage> {
         Expanded(
           flex: 3,
           child: Container(
-              color: Colors.blue,
-              child: Container(
-                alignment: const Alignment(-0.5, 0),
-                child: const Bird(birdSize: 50),
-              )),
+            color: Colors.blue,
+            child: Container(
+              child: Stack(
+                children: [
+                  Container(
+                    alignment: const Alignment(-0.5, 0),
+                    child: const Bird(
+                      birdSize: 50,
+                    ),
+                  ),
+                  Pipe(
+                    pipeHeight: 150,
+                    pipeWidth: _pipeWidth,
+                    xPipeAlignment: 0.5,
+                  ),
+                  Pipe(
+                    pipeHeight: 300,
+                    pipeWidth: _pipeWidth,
+                    xPipeAlignment: 0.5,
+                    isBottomPipe: true,
+                  ),
+                  Container(
+                    alignment: const Alignment(0, -0.3),
+                    child: const Text(
+                      'T A P  T O  P L A Y',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
         Expanded(
           child: Container(
